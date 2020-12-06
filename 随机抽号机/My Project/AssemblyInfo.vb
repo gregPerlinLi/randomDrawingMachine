17f2e059c3aa6c922555cf48346f49b337b2d102
gregPerlinLi <lihaolin13@outlook.com>
2020-12-06 23:56:42 +0800

    Add files via upload

---
+++ 随机抽号机/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的常规信息通过下列特性集
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+' 查看程序集特性的值
+
+<Assembly: AssemblyTitle("随机抽号机")>
+<Assembly: AssemblyDescription("此程序用于抽取号数,可自行设置总人数，并设有坑人模式，可防止重复抽号，设置时需输入密码,密码:{123456},如需更改密码请联系作者！(注意，该程序需要预先安装.NET Framework4.0以上版本)")>
+<Assembly: AssemblyCompany("gregPerlinLi")>
+<Assembly: AssemblyProduct("随机抽号机")>
+<Assembly: AssemblyCopyright("Copyright © gregPerlinLi 2016 AllRightReserved")>
+<Assembly: AssemblyTrademark("没有商标")>
+
+<Assembly: ComVisible(False)> 
+
+'如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+<Assembly: Guid("847fc324-bb65-4104-80fe-d5650bc5bb1c")>
+
+' 程序集的版本信息由下面四个值组成:
+'
+'      主版本
+'      次版本
+'      内部版本号
+'      修订号
+'
+' 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
+' 方法是按如下所示使用“*”:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("4.0.233.0976")>
+<Assembly: AssemblyFileVersion("2.6.113.7680")>
+<Assembly: NeutralResourcesLanguage("zh-Hans")>
